8d42d72a69f24b9df7c4850c2f74af2f585dca5a
kerrl34 <kerrl0478@student.faytechcc.edu>
2024-10-03 18:17:19 +0000

    p1hw1 - fixed input

---
+++ P1LAB/P1HW1_Kerr.py
@@ -0,0 +1,15 @@
+#CTI 110
+#P1HW1 - Calculator
+#Norris
+#10-3-24
+
+
+#num=int(input("Enter the integer as the base value: "))
+print("-" * 5, "Calculating Exponents", "-" * 5)
+x = int(input('Enter an integer as the base value: '))
+y = int(input("Enter an integer as the exponent: "))
+c = x ** y
+print(x,"raised to the power of", y, "is", c,"!!")
+
+print("-" * 5, "Addition and Subtraction", "-" * 5)
+
