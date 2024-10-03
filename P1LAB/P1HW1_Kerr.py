b2bce53088dafcc4db938fff77ac77203f77532b
kerrl34 <kerrl0478@student.faytechcc.edu>
2024-10-03 18:27:45 +0000

    p1hw1 - changed variable names and finished statement

--- P1LAB/P1HW1_Kerr.py
+++ P1LAB/P1HW1_Kerr.py
@@ -13,3 +13,11 @@ print(x,"raised to the power of", y, "is", c,"!!")
 
 print("-" * 5, "Addition and Subtraction", "-" * 5)
 
+start_integer = int(input("Enter a starting integer:"))
+add_integer = int(input("Enter an integer to add:"))
+sub_integer = int(input("Enter an integer to subtract:"))
+final_answer = start_integer + add_integer - sub_integer
+print(start_integer,"+", add_integer, "-", sub_integer, "is equal to:", final_answer)
+
+
+
