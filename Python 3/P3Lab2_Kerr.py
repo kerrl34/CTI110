df950b1fdeb9bc8b4cbb7a84805a60420f5f987a
kerrl34 <kerrl0478@student.faytechcc.edu>
2024-10-29 13:54:32 -0400

    Add files via upload

---
+++ Python 3/P3Lab2_Kerr.py
@@ -0,0 +1,36 @@
+# Liam Kerr
+# 10-29-24
+# CTI 110
+# P3Lab2
+
+import random
+def main():
+    print("Toad game")
+    print("roll from 1 to 6")
+   # die1 = int(input("First Die: "))
+   # die2 = int(input("Second Die: "))
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+
+    total = die1 + die2
+    print("Roll:", die1, "+", die2, "is", total)
+
+    # did we win or lose?
+    # 7 or 11 --> win, 2 or 12 --> lose, rest --> to be continued
+    if total == 7:
+        print("You win!")
+    elif total == 11:
+        print("You win!")
+    elif total == 2:
+        print("You lose.")
+    elif total == 12:
+        print("You lose.")
+    else:
+        print("Point was", total)
+        print("To be continued...")
+
+
+
+# start program
+main()
+
